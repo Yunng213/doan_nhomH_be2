a227444edfaa04b4abcc72e96359a6b6310eb894
nphongg2 <mthaooo913@gmail.com>
2025-05-16 15:20:48 +0700

    fix database

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,34 +3,17 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
-use App\Models\Product;
-use App\Models\Category;
-use App\Models\Latestproduct;
+use Illuminate\Support\Facades\Schema;
+
 class AppServiceProvider extends ServiceProvider
 {
-    /**
-     * Register any application services.
-     *
-     * @return void
-     */
     public function register()
     {
         //
     }
 
-    /**
-     * Bootstrap any application services.
-     *
-     * @return void
-     */
     public function boot()
     {
-        // view()->composer('*', function($view){
-        //     $category = Category::all();
-        //     $product = Product::all();
-        //     $data_category = Category::all(); 
-        //     $data_latestproduct = Latestproduct::all();
-        //         $view->with(compact('product','data_category','data_latestproduct','category'));
-        //   });
+        Schema::defaultStringLength(191);
     }
 }
